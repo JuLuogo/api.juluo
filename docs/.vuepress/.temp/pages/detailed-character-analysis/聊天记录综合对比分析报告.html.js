71c4c0b039a1929ed99b561a47e3b2f87e0f0127
JuLuogo <1576586736@qq.com>
2025-11-14 21:54:20 +0800

    chore: 添加静态资源文件并更新构建脚本
    添加多个图片和视频资源文件到文档目录
    更新package.json中的构建脚本配置
    清理临时文件中的热更新相关代码

--- docs/.vuepress/.temp/pages/detailed-character-analysis/聊天记录综合对比分析报告.html.js
+++ docs/.vuepress/.temp/pages/detailed-character-analysis/聊天记录综合对比分析报告.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/github/api.juluo/docs/.vuepress/.temp/pages/detailed-character-analysis/聊天记录综合对比分析报告.html.vue"
 const data = JSON.parse("{\"path\":\"/detailed-character-analysis/%E8%81%8A%E5%A4%A9%E8%AE%B0%E5%BD%95%E7%BB%BC%E5%90%88%E5%AF%B9%E6%AF%94%E5%88%86%E6%9E%90%E6%8A%A5%E5%91%8A.html\",\"title\":\"聊天记录综合对比分析报告\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1763123194000,\"contributors\":[{\"name\":\"JuLuogo\",\"username\":\"JuLuogo\",\"email\":\"1576586736@qq.com\",\"commits\":3,\"url\":\"https://github.com/JuLuogo\"}],\"changelog\":[{\"hash\":\"5365c993391bdde0d2f81570e62ca4e1b192871a\",\"time\":1763123194000,\"email\":\"1576586736@qq.com\",\"author\":\"JuLuogo\",\"message\":\"feat(docs): 新增橘络人物分析文档和资源库\"},{\"hash\":\"d3b48c37c719a0fe2d9cb274a38f115065778a1a\",\"time\":1762696556000,\"email\":\"1576586736@qq.com\",\"author\":\"JuLuogo\",\"message\":\"feat: 添加冬日树CG文件和使用说明\"},{\"hash\":\"0e8164240d7cc5eb86c5b195ccf92402a0351103\",\"time\":1762692652000,\"email\":\"1576586736@qq.com\",\"author\":\"JuLuogo\",\"message\":\"feat(详细人物分析): 新增人物关系分析文档集\"}]},\"filePathRelative\":\"detailed-character-analysis/聊天记录综合对比分析报告.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/github/api.juluo/docs/.vuepress/.temp/pages/index.html.vue"
 const data = JSON.parse("{\"path\":\"/\",\"title\":\"欢迎来到橘络人物分析文档\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"heroImage\":\"/loge/1.png\",\"heroText\":\"橘络人物分析文档\",\"heroAlt\":\"橘络人物分析项目\",\"features\":[{\"title\":\"全面分析\",\"details\":\"包含群聊分析、私人聊天、关系网络等多维度分析\"},{\"title\":\"专业文档\",\"details\":\"标准化的分析报告和详细的文档结构\"},{\"title\":\"高性能\",\"details\":\"基于VuePress构建，提供流畅的浏览体验\"}],\"actions\":[{\"text\":\"开始浏览\",\"link\":\"/detailed-character-analysis/\",\"type\":\"primary\"},{\"text\":\"了解项目\",\"link\":\"/guide/\"}],\"footer\":\"© 2025 橘络人物分析项目 | VuePress版本\"},\"git\":{\"updatedTime\":1763123194000,\"contributors\":[{\"name\":\"RuoShuigogo\",\"username\":\"RuoShuigogo\",\"email\":\"1576586736@qq.com\",\"commits\":2,\"url\":\"https://github.com/RuoShuigogo\"},{\"name\":\"JuLuogo\",\"username\":\"JuLuogo\",\"email\":\"1576586736@qq.com\",\"commits\":1,\"url\":\"https://github.com/JuLuogo\"}],\"changelog\":[{\"hash\":\"5365c993391bdde0d2f81570e62ca4e1b192871a\",\"time\":1763123194000,\"email\":\"1576586736@qq.com\",\"author\":\"JuLuogo\",\"message\":\"feat(docs): 新增橘络人物分析文档和资源库\"},{\"hash\":\"6a2e29908dad643491313efb63a2c806002a3e54\",\"time\":1717858851000,\"email\":\"1576586736@qq.com\",\"author\":\"RuoShuigogo\",\"message\":\"123\"},{\"hash\":\"d619a7952b27d2ea466fba8794f7b27a2db20c46\",\"time\":1717857535000,\"email\":\"1576586736@qq.com\",\"author\":\"RuoShuigogo\",\"message\":\"建设\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
